c2b3fa49391303ced41a9909f3a0596ad1ffdcd8
辰宿喵 <AruSeito@gmail.com>
2020-11-24 23:15:36 +0800

    Update init.js

--- bin/init.js
+++ bin/init.js
@@ -1,8 +1,2 @@
 #!/usr/bin/env node
-const axios = require('axios')
-const req = https.request('https://gitlab.szzbmy.com/api/v4/groups/147?private_token=sYnkJEEpwDk7F1Cg4VYc', (res) => {
-  res.on('data', d => {
-    process.stdout.write(d)
-  })
-})
-req.end()
+console.log(233333)
