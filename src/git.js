3a28b7341c6040475a972113eea1cb4ab54c9387
AruSeito <AruSeito@gmail.com>
2020-11-26 16:27:31 +0800

    feat: ✨ git模块相关操作封装

--- src/git.js
+++ src/git.js
@@ -1,13 +1,41 @@
 const download = require('download-git-repo')
+const childProcess = require('child_process');
+
 
 const downloadRepo = (repo, name, project = '') => {
   let finnalName = name;
   if (!!project) finnalName = `${name}_${project}`;
   return new Promise((resolve, reject) => {
     download(`direct:${repo}`, finnalName, { clone: true }, (err) => {
-      if (err) reject(err);
-      resolve('success');
+      if (!!err) reject(err);
+      resolve('download success');
     })
   })
 }
 
+const runCMD = (cmd) => {
+  return new Promise((resolve, reject) => {
+    childProcess.exec(cmd, (err) => {
+      if (!!err) reject(err);
+      resolve('run CMD success')
+    })
+  })
+}
+
+const openFolder = (name, project = '') => {
+  let finnalName = name;
+  if (!!project) finnalName = `${name}_${project}`;
+  return runCMD(`cd ${finnalName}`);
+}
+
+const checkoutBranch = (source) => {
+  return runCMD(`git checkout ${source}`);
+}
+
+const newBranch = (newBranchName) => {
+  return runCMD(`git checkout -b ${newBranchName}`);
+}
+
+const pushRemote = (newBranchName) => {
+  return runCMD(`git push --set-upstream origin ${newBranchName}`)
+}
