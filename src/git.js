daae4a292f9b2f7af040e31bebdbc9b3ce8e0eb1
AruSeito <AruSeito@gmail.com>
2020-11-26 09:14:06 +0800

    feat: ✨ 文件夹命名适配业务

--- src/git.js
+++ src/git.js
@@ -1,8 +1,10 @@
 const download = require('download-git-repo')
 
-const downloadRepo = (repo, name) => {
+const downloadRepo = (repo, name, project = '') => {
+  let finnalName = name;
+  if (!!project) finnalName = `${name}_${project}`;
   return new Promise((resolve, reject) => {
-    download(`direct:${repo}`, name, { clone: true }, (err) => {
+    download(`direct:${repo}`, finnalName, { clone: true }, (err) => {
       if (err) reject(err);
       resolve('success');
     })
